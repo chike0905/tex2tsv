5f0815b1795f54c3101db468018b169eb4bbca0e
chike0905 <chike@sfc.wide.ad.jp>
2018-12-29 15:11:37 +0900

    add tsv2tex

---
+++ tsv2tex.py
@@ -0,0 +1,21 @@
+from nltk.tokenize import sent_tokenize
+import sys
+import pandas as pd
+import math
+
+args = sys.argv
+
+tsv_data = pd.read_csv(args[1], delimiter='\t')
+
+out = ""
+for row in tsv_data["source"]:
+        if row != row:
+            out += "\n\n"
+        else:
+            out += row
+            if row[-1] == "}":
+                out += "\n"
+
+output_tex = open("output.tex", mode="a")
+output_tex.write(out)
+output_tex.close()
