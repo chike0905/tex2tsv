63091bc0d123b87b632f527695d631783b99a617
chike0905 <chike@sfc.wide.ad.jp>
2019-01-05 09:10:15 +0900

    show verbs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tex2csv
-Python script for convert LaTeX to TSV. By set the converted TSV file into Google Spreadsheet, you can use translate function per one sentence.
+Python script for convert LaTeX to TSV. By set the converted TSV file into Google Spreadsheet, you can check per one sentence.
 
 ## Environment
 - Python 3.6.5
@@ -15,7 +15,7 @@ pip install -r requirements.txt
 python tex2tsv.py input_file.tex output_file.tsv
 ```
 
-3. Import output.tsv Google Spread Sheet. Set 3rd Column `GOOGLETRANSLATE` function. Check translated sentences in your native language. Fix sentences.
+3. Import output.tsv Google Spread Sheet. Set Column D `=GOOGLETRANSLATE(B*,"en","ja")` and set Column E `=COUNTA(SPLIT(B*, " "))`. Check translated sentences in your native language. Fix sentences.
 ![GoogleSpreadSheet](https://github.com/chike0905/tex2tsv/blob/images/google.png)
 
 4. Download fixed spread sheet as TSV file. Convert TSV file to LaTeX file.
--- tex2tsv.py
+++ tex2tsv.py
@@ -1,4 +1,4 @@
-from nltk.tokenize import sent_tokenize
+import nltk
 import sys
 import pandas as pd
 
@@ -9,14 +9,25 @@ tex_data = open(args[1], "r")
 output = pd.DataFrame()
 
 tmp_sentence = []
+tmp_verbs = []
 for line in tex_data:
-    sentences = sent_tokenize(line)
+    sentences = nltk.sent_tokenize(line)
     if sentences == []:
         tmp_sentence.append(None)
-    for sentence in sentences:
-        tmp_sentence.append(sentence)
+        tmp_verbs.append(None)
+    else:
+        for sentence in sentences:
+            tmp_sentence.append(sentence)
+            words = nltk.word_tokenize(sentence)
+            words = nltk.pos_tag(words)
+            verbs = ""
+            for word in words:
+                if word[1][:2] == "VB":
+                    verbs += word[0] + ", "
+            tmp_verbs.append(verbs)
 
 output["source"] = tmp_sentence
+output["verbs"] = tmp_verbs
 output["translate"] = [None for a in range(0,len(tmp_sentence))]
 
 output.to_csv(args[2], sep='\t')
